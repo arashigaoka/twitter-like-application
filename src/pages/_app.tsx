cfd63de429b9b2326449ca81f3a7791a1c9b06b8
arashigaoka <bullet_of_lollypop@icloud.com>
2021-03-12 20:20:45 +0900

    setup hasura and graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,28 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps /*, AppContext */ } from 'next/app';
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+
+const createApolloClient = () => {
+  return new ApolloClient({
+    link: new HttpLink({
+      uri: 'http://localhost:8080/v1/graphql',
+    }),
+    cache: new InMemoryCache(),
+  });
+};
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  const client = createApolloClient();
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
