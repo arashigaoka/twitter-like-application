8a381383fc4f595246ae84ec16fab7c9d8b67e22
arashigaoka <bullet_of_lollypop@icloud.com>
2021-03-17 09:00:05 +0900

    refactor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,11 @@
 import 'tailwindcss/tailwind.css';
 import type { AppProps /*, AppContext */ } from 'next/app';
-import {
-  ApolloClient,
-  ApolloProvider,
-  HttpLink,
-  InMemoryCache,
-} from '@apollo/client';
-
-const createApolloClient = () => {
-  return new ApolloClient({
-    link: new HttpLink({
-      uri: 'http://localhost:8080/v1/graphql',
-    }),
-    cache: new InMemoryCache(),
-  });
-};
-
+import { DatabaseProvider } from '../hooks/use-database';
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  const client = createApolloClient();
   return (
-    <ApolloProvider client={client}>
+    <DatabaseProvider>
       <Component {...pageProps} />
-    </ApolloProvider>
+    </DatabaseProvider>
   );
 }
 
